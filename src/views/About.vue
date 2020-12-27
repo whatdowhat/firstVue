4db985dcb2267fb228bcf69d4cc0575b2014775a
whatdowhat <rhengh01@gmail.com>
2020-12-27 17:03:14 +0900

    'test2'

---
+++ src/views/About.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+//import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'About'
+  
+}
+
+console.log(process.env.BASE_URL);
+console.log('other');
+
+</script>
