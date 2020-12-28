2f1e5054461017ebabf86e9d1719362c55769dea
whatdowhat <rhengh01@gmail.com>
2020-12-29 02:28:54 +0900

    '1229'

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hell">
     <h1>{{ msg }}</h1>
+    <div style="color:red">{{name}}</div>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -34,7 +35,8 @@
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String
+    msg: String,
+    name:String
   }
 }
 </script>
